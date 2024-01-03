0f1f1cb9b39d3fa4d1e0e7727f27d360864c1104
ldessy <laurent5dessy@gmail.com>
2024-01-03 02:31:14 +0100

    add log for roles and teams

--- app/api/teams/route.ts
+++ app/api/teams/route.ts
@@ -4,7 +4,7 @@ export const GET = async () => {
     try {
         const teams = await prisma.team.findMany()
         return NextResponse.json(teams, {status: 200})
-    }catch (e) {
-        return NextResponse.json({message: "Something went wrong"}, {status: 500})
+    }catch (e: any) {
+        return NextResponse.json({message: "Something went wrong", error: e.message}, {status: 500})
     }
 }
