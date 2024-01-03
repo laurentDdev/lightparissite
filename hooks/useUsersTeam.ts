a7ac8afeea4733e6157a06bafedf91c8d42592f5
ldessy <laurent5dessy@gmail.com>
2024-01-03 02:50:51 +0100

    change querykey for team

--- hooks/useUsersTeam.ts
+++ hooks/useUsersTeam.ts
@@ -2,7 +2,7 @@ import {useQuery} from "react-query";
 import axios from "axios";
 
 export const useUsersTeam = () => {
-  return useQuery("teams", async () => {
+  return useQuery("usersTeam", async () => {
     const { data } = await axios.get("/api/users/team");
     return data;
   });
